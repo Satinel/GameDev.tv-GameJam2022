840a07af52b41cafc81facd5afc8018781208694
Nimufu <angelonmyknees@gmail.com>
2022-05-21 01:00:46 -0300

    Basic movements clamped because I'm not going to figure out how to keep the player from clipping through the walls otherwise

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,33 +5,59 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] int moveSpeed = 10;
-    [SerializeField] int rotationSpeed = 200;
-    
+    [SerializeField] float rotationSpeed = 0.15f;
+    float xRange = 29;
+    float zRange = 16;
     
     void Start()
     {
         
     }
 
-    void MovePlayer() //TODO maybe find some way to do this that actually works thanks
+    //https://answers.unity.com/questions/803365/make-the-player-face-his-movement-direction.html
+    void MovePlayer() 
     {
         float moveLeftRight = Input.GetAxis("Horizontal");
         float moveUpDown = Input.GetAxis("Vertical");
         Vector3 movePlayer = new Vector3(moveLeftRight, 0, moveUpDown);
-        transform.rotation = Quaternion.LookRotation(movePlayer);
-        transform.Translate(movePlayer * Time.deltaTime * moveSpeed, Space.World);
-        
+        if (movePlayer != Vector3.zero)
+        {
+            transform.rotation = 
+            Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movePlayer), rotationSpeed);
+        }
+        transform.Translate(movePlayer * Time.fixedDeltaTime * moveSpeed, Space.World); 
     }
 
-    void RotatePlayer()
+    float xThrow;
+    float zThrow;
+
+    void ProcessTranslation()
     {
-        float playerRotate = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
-        transform.Rotate(0,playerRotate,0);
+        xThrow = Input.GetAxis("Horizontal");
+        zThrow = Input.GetAxis("Vertical");
+        Vector3 lookDirection = new Vector3(xThrow, 0, zThrow);
+        if (lookDirection != Vector3.zero)
+        {
+            transform.rotation = 
+            Quaternion.Slerp(transform.rotation, 
+                            Quaternion.LookRotation(lookDirection), 
+                            rotationSpeed);
+        }
+
+        float xOffset = xThrow * Time.deltaTime * moveSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float zOffset = zThrow * Time.deltaTime * moveSpeed;
+        float rawZPos = transform.localPosition.z + zOffset;
+        float clampedYPos = Mathf.Clamp(rawZPos, -zRange, zRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, clampedYPos);
     }
     
-    void Update()
+    void FixedUpdate()
     {
-        MovePlayer();
-        // RotatePlayer();
+        // MovePlayer();
+        ProcessTranslation();
     }
 }
