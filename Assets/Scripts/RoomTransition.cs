e94720f6f953335a191ecf74fda048f319d823a7
Nimufu <angelonmyknees@gmail.com>
2022-05-22 17:56:29 -0300

    Rat can now move legs, head and tail independently and has idle animation

--- Assets/Scripts/RoomTransition.cs
+++ Assets/Scripts/RoomTransition.cs
@@ -7,17 +7,15 @@ public class RoomTransition : MonoBehaviour
     [SerializeField] float horizontalDirection = -62;
     [SerializeField] float verticalDirection = 0;
     [SerializeField] Transform dungeon;
+    
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            // FindObjectOfType<PlayerMovement>().enabled = false;
             dungeon.Translate(horizontalDirection,0,verticalDirection);
             Transform playerParent = other.transform.parent;
             Debug.Log(playerParent.name);
             playerParent.Translate(horizontalDirection,0,verticalDirection);
-            // FindObjectOfType<PlayerMovement>().enabled = true;
-            // other.transform.Translate(-25,0,0, Space.World);
         }
     }
 }
