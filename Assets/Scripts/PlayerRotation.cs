44fc7ccfe353ff5485f9ff7ac0ad6f6682dc095c
Nimufu <angelonmyknees@gmail.com>
2022-05-23 00:18:42 -0300

    Player Idle and Running animations, gravity works but rigidbody collisions are very bad

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
+
     //https://answers.unity.com/questions/803365/make-the-player-face-his-movement-direction.html
     [SerializeField] float rotationSpeed = 0.15f;
+    [SerializeField] Animation runAnimation;
     float xThrow;
     float zThrow;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void RotatePlayer()
     {
         xThrow = Input.GetAxis("Horizontal");
@@ -19,6 +28,11 @@ public class PlayerRotation : MonoBehaviour
             Quaternion.Slerp(transform.rotation, 
                             Quaternion.LookRotation(lookDirection), 
                             rotationSpeed);
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
         }
     }
     
