b3925ab9e4400a5513355f2b806e641390e09cb2
Nimufu <angelonmyknees@gmail.com>
2022-05-25 21:56:27 -0300

    Got some cool error about some thing I know nothing about: UnityEditor.Graphs.Edge.WakeUp ()

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [SerializeField] int damage = 1;
+    [SerializeField] int size = 1;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponent<PlayerHealth>().DecreaseHealth(damage * size);
+            Debug.Log("I hit " + other.gameObject.name + " for " + damage * size);
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,6 +6,7 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int maxHealth = 3;
     [SerializeField] int hitPoints;
+    bool isSquished = false;
 
     public void IncreaseHealth()
     {
@@ -15,15 +16,53 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    public void DecreaseHealth(int damage)
+    {
+        if (isSquished)
+        {
+            return;
+        }
+        hitPoints -= damage;
+        //animator.SetTrigger("tookDamage"); TODO make a damage animation for player
+        //TODO Play a sound
+        if(hitPoints <= 0)
+        {
+            Debug.Log("You Died");
+            //TODO make a life depletion method
+            //BecomeGhost();
+        }
+    }
+
     //TODO IncreaseSize()
     
-    // Start is called before the first frame update
+    public void SquishThatRat()
+    {
+        if (!isSquished)
+        {
+            float originalSizeX = transform.localScale.x;
+            float originalSizeY = transform.localScale.y;
+            float originalSizeZ = transform.localScale.z;
+            transform.localScale = new Vector3(originalSizeX, originalSizeY*0.1f, originalSizeZ);
+            isSquished = true;
+            Invoke("Unsquish", 2f);
+        }
+    }
+
+    void Unsquish()
+    {
+        float originalSizeX = transform.localScale.x;
+        float originalSizeY = transform.localScale.y;
+        float originalSizeZ = transform.localScale.z;
+        transform.localScale = new Vector3(originalSizeX, originalSizeY*10f, originalSizeZ);
+        isSquished = false;
+    }
+    
     void Start()
     {
         hitPoints = maxHealth;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
