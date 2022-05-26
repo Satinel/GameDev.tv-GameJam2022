e9064ccca1510c23d2d21dc7acb85174e1421014
Nimufu <angelonmyknees@gmail.com>
2022-05-25 22:03:59 -0300

    The error went away and the squish attack works!

---
+++ Assets/Scripts/EnemySquishAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySquishAttack : MonoBehaviour
+{
+    [SerializeField] int damage = 1;
+    [SerializeField] int size = 1;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponent<PlayerHealth>().DecreaseHealth(damage * size);
+            other.GetComponent<PlayerHealth>().SquishThatRat();
+            Debug.Log("I squished " + other.gameObject.name + "!");
+        }
+    }
+}
